8990cfd67811f5c5b251e933a7cf184d472441c1
Forrest <forrestburrelsman@gmail.com>
2021-02-20 21:59:08 -0600

    First commit

---
+++ src/main/java/net/wattever/wattever/WatteverApplication.java
@@ -0,0 +1,13 @@
+package net.wattever.wattever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatteverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatteverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/wattever/wattever/WatteverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wattever.wattever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatteverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
